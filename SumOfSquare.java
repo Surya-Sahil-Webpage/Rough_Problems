8b00108ed2c690358242fc0a600451ba990837fc
Surya Sahil Vadapalli <surya.sahil.vadapalli@ibm.com>
2025-09-12 12:25:15 +0530

    IBM changes 2

---
+++ SumOfSquare.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class SumOfSquare {
+    public static int totalSquare(int n) {
+        return (n * (n + 1) * (2 * n + 1)) / 6;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        System.out.println(totalSquare(n));
+    }
+}
